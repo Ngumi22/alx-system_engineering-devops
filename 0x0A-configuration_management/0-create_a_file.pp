b8bb99e72933fc9c044a9da7b68aae3616b061cf
Ngumi22 <mwaurapngumi@gmail.com>
2024-04-20 02:16:34 +0300

    Configuration mngt

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,8 @@
+#Creates a file called school in /tmp
+file { '/tmp/school':
+  ensure  => file,
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
+}
